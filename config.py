a2f5bc9190e7c61ae87ca853edb01edac5a70aae
Erenluffy <luffyravi2000@gmail.com>
2025-12-04 01:05:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot")
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://teddugovardhan544_db_user:WVjIA96jQ31net0j@cluster0.kwkkleo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-    BOT_OWNER = int(environ.get("BOT_OWNER", "6317211079"))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "7259016766"))
     
     # Add this line - Your secret dump channel ID
     SECRET_DUMP_CHANNEL = int(environ.get("SECRET_DUMP_CHANNEL", -1001234567890))
@@ -18,4 +18,5 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
+
 
