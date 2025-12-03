338499f7a5c18f9c6f922f806fba16461db94beb
Erenluffy <luffyravi2000@gmail.com>
2025-12-04 00:25:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 from os import environ
-from typing import Optional
 
 class Config:
     API_ID = int(environ.get("API_ID", ""))
@@ -10,7 +9,7 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     BOT_OWNER = int(environ.get("BOT_OWNER", "6317211079"))
     
-    # Secret dump channel ID (add your channel ID here)
+    # Add this line - Your secret dump channel ID
     SECRET_DUMP_CHANNEL = int(environ.get("SECRET_DUMP_CHANNEL", -1001234567890))
     
 class temp(object): 
@@ -19,6 +18,3 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-    
-    # For tracking dump channel operations
-    dump_operations = {}
